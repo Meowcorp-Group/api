814df4d088aa6db9ead9d11fc2ce7d1f8aae0dad
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-07-16 14:19:03 +0700

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use axum::{Router, routing::get, response::Html};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = Router::new()
+		.route("/", get(root));
+
+	let listener = tokio::net::TcpListener::bind("127.0.0.1:3030").await?;
+	println!("listening on http://{}", listener.local_addr().unwrap());
+	axum::serve(listener, app).await?;
+
+	Ok(())
+}
+
+async fn root() -> Html<&'static str> {
+	Html("<h1>Hello, World!</h1>")
 }
